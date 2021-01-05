3ed6076ffa0c151392ef1a176283fc7c9a37369b
Frederick De Baene <frederickdebaene@MacBook-Pro-van-Frederick.local>
2021-01-05 22:39:58 +0100

    RNA Splicing

---
+++ splc.py
@@ -0,0 +1,32 @@
+# ROSALIND INFORMATION - ID : SPLC - TITLE : RNA SPLICING
+
+import fasta
+import rna
+import prot
+
+def splicing(sequences):
+
+    sequence_keys = sequences.keys()
+    introns = []
+    counter = 0
+
+    for i in sequences.keys():
+        if counter == 0:
+            coding_strand = sequences[i]
+            counter += 1
+        else:
+            introns.append(sequences[i])
+
+    for i in introns:
+        coding_strand = coding_strand.replace(i, "")
+
+    return coding_strand
+
+if __name__ == "__main__":
+
+    file_directory = str(input("Enter the FASTA file directory : "))
+    sequences = fasta.read_fasta_file(file_directory)
+    coding_strand = splicing(sequences)
+    mrna = rna.transcribe_as_coding_strand(coding_strand)
+    protein = prot.translate(mrna)
+    print(protein == "MVYIADKQHVASREAYGHMFKVCA")
